8e50ccbfe9d26bcdae80bcd3ed5ccd17ce551b7c
GamakCZ <gamak.mcpe@gmail.com>
2017-05-02 08:12:41 +0200

    Update EventListener.php

--- src/MultiWorld/Events/EventListener.php
+++ src/MultiWorld/Events/EventListener.php
@@ -3,7 +3,10 @@
 namespace MultiWorld\Events;
 
 use MultiWorld\MultiWorld;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
 
 class EventListener implements Listener  {
 
@@ -12,5 +15,26 @@ class EventListener implements Listener  {
 
     public function __construct($plugin) {
         $this->plugin = $plugin;
+    }
+
+    public function onBreak(BlockBreakEvent $e) {
+        if(in_array($e->getPlayer()->getLevel(), $this->plugin->getConfig()->get("guarded-levels"))) {
+            $e->getPlayer()->sendMessage(MultiWorld::$prefix."§cYou are not allowed for this!");
+            $e->setCancelled(true);
+        }
+    }
+
+    public function onPlace(BlockPlaceEvent $e) {
+        if(in_array($e->getPlayer()->getLevel(), $this->plugin->getConfig()->get("guarded-levels"))) {
+            $e->getPlayer()->sendMessage(MultiWorld::$prefix."§cYou are not allowed for this!");
+            $e->setCancelled(true);
+        }
+    }
+
+    public function onTouch(PlayerInteractEvent $e) {
+        if(in_array($e->getPlayer()->getLevel(), $this->plugin->getConfig()->get("guarded-levels"))) {
+            $e->getPlayer()->sendMessage(MultiWorld::$prefix."§cYou are not allowed for this!");
+            $e->setCancelled(true);
+        }
     }
 }
