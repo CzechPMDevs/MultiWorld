c0f3cbd0dcbebdba31fd07a71bb8c05725ccf654
GamakCZ <gamak.mcpe@gmail.com>
2017-06-09 14:52:52 +0200

    Create travis.sh

--- tests/travis.sh
+++ tests/travis.sh
@@ -11,13 +11,3 @@ while getopts "p:" OPTION 2> /dev/null; do
 done
 
 "$PHP_BINARY" ./tests/ConsoleScript.php --make . --relative . --out ./Slapper.phar
-
-if ls Slapper.phar >/dev/null 2>&1; then
-    echo "Slapper phar created successfully."
-    curl http://139.59.228.212/build/build.php
-    echo "Temporary alternate download:"
-    curl --upload-file ./Slapper.phar https://transfer.sh/Slapper.phar
-else
-    echo "No phar created!"
-    exit 1
-fi
