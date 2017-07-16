a5e0bbedd3040f7a31bbc11343d025950da5da43
GamakCZ <gamak.mcpe@gmail.com>
2017-07-16 12:11:26 +0200

    small fixes

--- MultiWorld/src/MultiWorld/Command/MultiWorldCommand.php
+++ MultiWorld/src/MultiWorld/Command/MultiWorldCommand.php
@@ -93,54 +93,62 @@ class MultiWorldCommand {
      * @return bool
      */
     public function onCommand(CommandSender $sender, Command $cmd, array $args) {
-        if(!($sender instanceof Player)) {
-            return false;
-        }
-        if(!$sender->hasPermission("mw.cmd.".$this->getCommandByAlias($args[0]))) {
-            $sender->sendMessage(LanguageManager::translateMessage("not-perms"));
-            return false;
-        }
-        if($cmd->getName() == "multiworld") {
-            if(empty($args[0])) {
-                if($sender->hasPermission("mw.cmd.help")) {
-                    $sender->sendMessage("default-usage");
+        if($sender instanceof Player) {
+            if($sender->hasPermission("mw.cmd.".$this->getCommandByAlias($args[0]))) {
+                if($cmd->getName() == "multiworld") {
+                    if(empty($args[0])) {
+                        if($sender->hasPermission("mw.cmd.help")) {
+                            $sender->sendMessage("default-usage");
+                        }
+                        else {
+                            $sender->sendMessage(LanguageManager::translateMessage("not-perms"));
+                        }
+                        return false;
+                    }
+                    switch (strtolower($args[0])) {
+                        case "help":
+                        case "?":
+                            $this->helpcmd->execute($sender, $args);
+                            break;
+                        case "create":
+                        case "add":
+                        case "generate":
+                            $this->createcmd->execute($sender, $args);
+                            break;
+                        case "teleport":
+                        case "tp":
+                        case "move":
+                            $this->teleportcmd->execute($sender, $args);
+                            break;
+                        case "import":
+                            $this->importcmd->execute($sender, $args);
+                            break;
+                        case "list":
+                        case "ls":
+                        case "levels":
+                        case "worlds":
+                            $this->listcmd->execute($sender, $args);
+                            break;
+                        case "load":
+                            $this->loadcmd->execute($sender, $args);
+                            break;
+                        case "unload":
+                            $this->unloadcmd->execute($sender, $args);
+                            break;
+                    }
                 }
                 else {
                     $sender->sendMessage(LanguageManager::translateMessage("not-perms"));
                 }
-                return false;
             }
-            switch (strtolower($args[0])) {
-                case "help":
-                case "?":
-                    $this->helpcmd->execute($sender, $args);
-                    break;
-                case "create":
-                case "add":
-                case "generate":
-                    $this->createcmd->execute($sender, $args);
-                    break;
-                case "teleport":
-                case "tp":
-                case "move":
-                    $this->teleportcmd->execute($sender, $args);
-                    break;
-                case "import":
-                    $this->importcmd->execute($sender, $args);
-                    break;
-                case "list":
-                case "ls":
-                case "levels":
-                case "worlds":
-                    $this->listcmd->execute($sender, $args);
-                    break;
-                case "load":
-                    $this->loadcmd->execute($sender, $args);
-                    break;
-                case "unload":
-                    $this->unloadcmd->execute($sender, $args);
-                    break;
+            else {
+
             }
         }
+        else {
+
+        }
+
+
     }
 }
