f301955111a51d29d428e196783e07c3c5192bef
VixikHD <gamak.pmmp@gmail.com>
2021-05-30 18:14:56 +0200

    It's 2021

--- src/czechpmdevs/multiworld/generator/normal/biome/Ocean.php
+++ src/czechpmdevs/multiworld/generator/normal/biome/Ocean.php
@@ -2,7 +2,7 @@
 
 /**
  * MultiWorld - PocketMine plugin that manages worlds.
- * Copyright (C) 2018 - 2020  CzechPMDevs
+ * Copyright (C) 2018 - 2021  CzechPMDevs
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
