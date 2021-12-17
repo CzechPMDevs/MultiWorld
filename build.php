711d7c3955a25a2ae0d93244ba86985f5db431cf
VixikHD <gamak.pmmp@gmail.com>
2021-12-17 13:38:34 +0100

    Fix build script not adding libpmform

--- build.php
+++ build.php
@@ -1,10 +1,24 @@
 <?php
 
-declare(strict_types=1);
-
 /**
- * Build script
+ * MultiWorld - PocketMine plugin that manages worlds.
+ * Copyright (C) 2018 - 2021  CzechPMDevs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+declare(strict_types=1);
 
 // For example C:/pmmp-server/plugins/MultiWorld.phar
 const CUSTOM_OUTPUT_PATH = "";
@@ -25,13 +39,18 @@ cleanDirectory($to);
 // Copying new files...
 copyDirectory($from . "src", $to . "src");
 copyDirectory($from . "resources", $to . "resources");
-copyDirectory($from . "vendor/czechpmdevs/libpmform/src", $to . "src/czechpmdevs/libpmform");
+copyDirectory($from . "vendor/czechpmdevs/libpmform/src", $to . "src/");
 
-$description = yaml_parse_file($from . "plugin.yml");
+$description = (array)yaml_parse_file($from . "plugin.yml");
 yaml_emit_file($to . "plugin.yml", $description);
 
+$buildVersion = strval($description["version"] ?? "unknown");
+
 // Defining output path...
-$outputPath = CUSTOM_OUTPUT_PATH == "" ? getcwd() . DIRECTORY_SEPARATOR . "out" . DIRECTORY_SEPARATOR . "MultiWorld_{$description["version"]}_dev.phar" : CUSTOM_OUTPUT_PATH;
+$outputPath = CUSTOM_OUTPUT_PATH === "" ?
+	getcwd() . DIRECTORY_SEPARATOR . "out" . DIRECTORY_SEPARATOR . "MultiWorld_{$buildVersion}_.phar" :
+	CUSTOM_OUTPUT_PATH;
+
 @unlink($outputPath);
 
 // Generate phar
@@ -45,14 +64,14 @@ if(COMPRESS_FILES) {
 printf("Plugin built in %s seconds! Output path: %s\n", round(microtime(true) - $startTime, 3), $outputPath);
 
 function copyDirectory(string $from, string $to): void {
-    mkdir($to, 0777, true);
+    @mkdir($to, 0777, true);
 
-    $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($from, RecursiveDirectoryIterator::SKIP_DOTS), RecursiveIteratorIterator::SELF_FIRST);
+    $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($from, FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::SELF_FIRST);
     /** @var SplFileInfo $fileInfo */
     foreach ($files as $fileInfo) {
         $target = str_replace($from, $to, $fileInfo->getPathname());
         if($fileInfo->isDir()) {
-            mkdir($target, 0777, true);
+            @mkdir($target, 0777, true);
         } else {
             $contents = file_get_contents($fileInfo->getPathname());
             file_put_contents($target, $contents);
@@ -61,7 +80,7 @@ function copyDirectory(string $from, string $to): void {
 }
 
 function cleanDirectory(string $directory): void {
-    $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory, RecursiveDirectoryIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST);
+    $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory, FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST);
     /** @var SplFileInfo $fileInfo */
     foreach ($files as $fileInfo) {
         if($fileInfo->isDir()) {
